853d6f7274adf851150dbdfdd2c53fedbd5478f2
wangtao <wangtao19910316@163.com>
2018-04-10 16:57:54 +0800

    html change

--- mysite/views.py
+++ mysite/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 
+user_list = [
+    {'user':'jack','pwd':'abc'},
+    {'user':'tom','pwd':'ABC'},
+]
+
 def index(request):
+    if request.method == 'post':
+        username = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        print(username,password)
     # request.POST
     # request.GET
-    return render(request,'index.html')
+    return render(request,'index.html',{'data':user_list})
