620c690c8330510e6c57a4ea2dfbc56a043923ff
wangtao <wangtao19910316@163.com>
2018-04-12 10:19:40 +0800

    html change

--- mysite/views.py
+++ mysite/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from mysite import models
 # Create your views here.
 
-user_list = [
-    {'user':'jack','pwd':'abc'},
-    {'user':'tom','pwd':'ABC'},
-]
 
 def index(request):
     if request.method == 'post':
         username = request.POST.get('username',None)
         password = request.POST.get('password',None)
-        print(username,password)
-    # request.POST
-    # request.GET
+    #添加数据到数据库
+    models.UserInfo.objects.create(user=username,pwd=password)
+    #从数据库中读取所有数据
+    user_list = models.UserInfo.objects.all()
+
     return render(request,'index.html',{'data':user_list})
